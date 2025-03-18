0df0c8118fa60ab05e144e7583539232cbde9c3b
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-03-18 16:41:18 +0100

    made api key secret XD

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,4 +1,9 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 def get_animal_json(name):
     """
@@ -10,7 +15,7 @@ def get_animal_json(name):
         list or None: A list of animal data in JSON format if successful, otherwise None.
     """
     api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
-    response = requests.get(api_url, headers={'X-Api-Key': 'e4kugb+adSUD+i3UPMNCKQ==hETJEYy9dgplQWPV'})
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
         return response.json()
     else:
